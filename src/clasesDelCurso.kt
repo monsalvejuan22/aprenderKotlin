8b98a8edace99cc6a1e2c2edf4d793bd395db271
Juan Monsalve <monsalvejuanpablo22@gmail.com>
2021-04-11 12:26:48 -0400

    Comienzo del curso Kotlin basico

---
+++ src/clasesDelCurso.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var a = 5
+    var b = 6
+    println(a.plus(b)) // Suma a + b y lo imprime
+    println(a.compareTo(b)) // compara si a es mayor muestra 1, si es igual 0, y si a es menor muestra -1
+
+
+
+
+
+}
