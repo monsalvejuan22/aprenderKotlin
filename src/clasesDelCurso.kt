66c87d24bd45db4b4d0a49ef20339c6c87b7d800
Juan Monsalve <monsalvejuanpablo22@gmail.com>
2021-04-11 12:32:04 -0400

    Crear rango he imprimirlo

--- src/clasesDelCurso.kt
+++ src/clasesDelCurso.kt
@@ -1,11 +1,21 @@
 fun main(args: Array<String>) {
+
+  /*
     var a = 5
     var b = 6
     println(a.plus(b)) // Suma a + b y lo imprime
     println(a.compareTo(b)) // compara si a es mayor muestra 1, si es igual 0, y si a es menor muestra -1
 
 
+   */
 
 
+    // rangos crea rango del 1..10 y lo imprime
+
+    var oneToTen = 1..10
+
+    for (i in oneToTen){
+        println(i)
+    }
 
 }
